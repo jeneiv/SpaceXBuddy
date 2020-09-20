b0aa1165691365add798aa47cfe0ecea0c650b09
Viktor Jenei <viktor.jenei@burberry.com>
2020-09-20 18:46:55 +0200

    Minor Changes
    AppDelegate Added to handle app lifecycle/notification callbacks

---
+++ Source/SpaceX Buddy/SpaceXBuddyAppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SpaceXBuddyAppDelegate.swift
+//  SpaceX Buddy
+//
+//  Created by Jenei Viktor on 2020. 09. 20..
+//
+
+import Foundation
+import UIKit
+
+class SpaceXBuddyAppDelegate : NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        true
+    }
+}
